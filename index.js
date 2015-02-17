7e7108576404351b09221f58c0aa6a53adb95757
Marek ‘saji’ Augustynowicz <marek.aug@gmail.com>
2015-02-17 16:07:12 +0100

    CSV → PO

--- index.js
+++ index.js
@@ -38,6 +38,26 @@ function loadPoFile (poFilePath)
     });
 }
 
+function loadCsvFile (csvFilePath)
+{
+    var csvData = [];
+    return new Promise(function (resolve, reject) {
+        csv.fromPath(
+            csvFilePath,
+            {
+                headers: true
+            }
+        )
+            .on('data', function (row) {
+                csvData.push(row);
+            })
+            .on('end', function () {
+                resolve(csvData);
+            });
+    });
+}
+
+
 function transformPoToCsv (poData)
 {
     return new Promise(function (resolve, reject) {
@@ -59,6 +79,11 @@ function transformPoToCsv (poData)
             }
         );
     });
+}
+
+function transformCsvToPo (csvData)
+{
+    return Promise.resolve(csvData.map(transformCsvRowToPoItem));
 }
 
 function transformPoItemToCsvRow (nplurals, item)
@@ -84,9 +109,76 @@ function transformPoItemToCsvRow (nplurals, item)
     return row;
 }
 
+function transformCsvRowToPoItem (row)
+{
+    var i;
+    var item = new PO.Item();
+    var plural = false;
+
+    item.msgid             = row.msgid             || item.msgid;
+    item.msgid_plural      = row.msgid_plural      || item.msgid_plural;
+    item.references        = row.references.split('\n')        || item.references;
+    item.extractedComments = row.extractedComments.split('\n') || item.extractedComments;
+    row.flags.split('\n').forEach(function (flag) {
+        item.flags[flag] = true;
+    });
+
+    for (i = 0 ; 'msgstr[' + i + ']' in row ; i += 1)
+    {
+        item.msgstr[i] = row['msgstr[' + i + ']'];
+        if (i && item.msgstr[i])
+        {
+            plural = true;
+        }
+    }
+    if (! plural)
+    {
+        item.msgstr = [ item.msgstr[0] ];
+    }
+
+    return item;
+}
+
 function writeCsvOutput (data)
 {
     console.log(data);
+}
+
+
+function mergeIntoPo (datas)
+{
+    var target = datas.shift();
+    var targetItemsByMsgId = {};
+    target.items.forEach(function (item) {
+        targetItemsByMsgId[item.msgid] = item;
+    });
+
+    datas.forEach(function (itemsToMerge) {
+        itemsToMerge.forEach(function (item) {
+            var targetItem = targetItemsByMsgId[item.msgid];
+            if (! targetItem)
+            {
+                throw Error('Item “' + item.msgid + '” does not exist in target PO file.');
+            }
+            if (targetItem.msgid_plural !== item.msgid_plural)
+            {
+                throw Error('msgid_plural mismatch for “' + msgid + '”');
+            }
+            targetItem.msgstr = item.msgstr;
+            targetItem.flags  = item.flags;
+        });
+    });
+
+    return Promise.resolve(target);
+}
+
+function writePoOutput (poData)
+{
+    try
+    {
+    console.log('' + poData);
+    }
+    catch (e) { throw e; }
 }
 
 function printHelp ()
@@ -102,10 +194,25 @@ function printPoAsCsv (poFilePath)
         .catch(throwUp);
 }
 
+function mergeCsvIn (poFilePath, csvFilePath)
+{
+    Promise.all([
+        loadPoFile(poFilePath),
+        loadCsvFile(csvFilePath)
+            .then(transformCsvToPo)
+    ])
+        .then(mergeIntoPo)
+        .then(writePoOutput)
+        .catch(throwUp);
+}
 
 if (3 === process.argv.length)
 {
     printPoAsCsv(process.argv[2]);
+}
+else if (4 === process.argv.length)
+{
+    mergeCsvIn(process.argv[2], process.argv[3]);
 }
 else
 {
