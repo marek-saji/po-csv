b4df5c6a62477450ffc4eb7d4feab27a7df0661b
Marek ‘saji’ Augustynowicz <marek.aug@gmail.com>
2016-03-30 11:52:57 +0200

    Split tests into directories

--- run-tests.sh
+++ run-tests.sh
@@ -1,6 +1,47 @@
 #!/bin/sh
 
-set -e
+if locale | grep LC_CTYPE | grep -Eqi utf.?8
+then
+    FAIL=✗
+    WIN=✓
+else
+    FAIL=error
+    WIN=passed
+fi
 
-./index.js tests/input.po | diff - tests/output.csv
-./index.js tests/input.po tests/translated.csv | diff - tests/translated.po
+if [ -t 1 ]
+then
+    FAIL="\033[31m${FAIL}\033[m"
+    WIN="\033[32m${WIN}\033[m"
+fi
+
+ALL_ERRORS=
+for TEST_PATH in tests/*
+do
+    printf "${TEST_PATH} "
+
+    ERROR=
+    if ! ./index.js "${TEST_PATH}"/input.po | diff - "${TEST_PATH}"/output.csv
+    then
+        ERROR=1
+        printf "${FAIL}"
+    fi
+    if !  ./index.js "${TEST_PATH}"/input.po "${TEST_PATH}"/input.csv | diff - "${TEST_PATH}"/output.po
+    then
+        ERROR=1
+        printf "${FAIL}"
+    fi
+    if [ -z "${ERROR}" ]
+    then
+        printf "${WIN}"
+    fi
+
+    echo
+
+    ALL_ERRORS="${ALL_ERRORS}${ERROR}"
+done
+
+if [ -n "${ALL_ERRORS}" ]
+then
+    exit 1
+fi
